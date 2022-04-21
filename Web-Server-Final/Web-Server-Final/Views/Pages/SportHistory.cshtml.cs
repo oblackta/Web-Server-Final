43473c8de709154bf8b980951bb3eb7082006126
ascender50 <apugh734@gmail.com>
2022-04-21 13:07:08 -0400

    Base line for the site

---
+++ Web-Server-Final/Web-Server-Final/Views/Pages/SportHistory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_Server_Final.Views.Pages
+{
+    public class SportHistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
